6f0e02169aa57b6636f64a8fecd0ba891912e194
antonbergenudd <antonbergenudd@hotmail.com>
2022-03-30 13:31:09 +0200

    Fix normalized distribution problem

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,58 +37,120 @@ class User extends Authenticatable
         return $this->belongsToMany(Article::class)->withPivot('relevance', 'understandability', 'length');
     }
 
+    /**
+    * Helper function to print summary of distribution
+    */
+    private function printBalance($user_profile, $b, $check) {
+        $userArraySum = array_sum($user_profile);
+
+        if($check) {
+            if(bccomp($userArraySum, 1.0, 2) <> 0) {
+                if($b) {
+                    var_dump("Check before sum: ". $userArraySum);
+                } else {
+                    var_dump("Check after sum: ". $userArraySum);
+                }
+            }
+        } else {
+            if($b) {
+                var_dump("Before sum: ". $userArraySum);
+            } else {
+                var_dump("After sum: ". $userArraySum);
+            }
+        }
+    }
+
+    /**
+     * Helper function to calculate updated user profile
+     */
     private function calculateUserProfile($user, $articles, $update) {
         $num_topics = count(json_decode($articles->first()->label_dist));
+        $other_num_topics = $num_topics - 1;
         $user_profile = isset($user->profile) ? json_decode($user->profile) : array_fill(0, $num_topics, (100/$num_topics)/100);
         $total_num_answers = count($articles);
 
         // Loop all articles and compute average label dist for user profile
         foreach($articles as $i => $article) {
+
             // Get article distribution
             $article_dist = json_decode($article->label_dist);
 
             // Add each weighted label distribution over an article to collected array (rel score * single label dist)
             $total_diff = [];
             foreach($article_dist as $j => $single_topic_dist) {
+                $userArraySum = array_sum($user_profile);
+
                 // Get abs. difference between profiles
                 $diff = abs($user_profile[$j] - $single_topic_dist);
                 $total_diff[] = $diff;
 
                 // Decrease impact of articles the more you answer
                 // Roughly every other iteration (30 articles)
-                $gradient = $total_num_answers >= 30 ? (1 / ($total_num_answers/30)) : 1;
+                $gradient = $total_num_answers >= 30 ? (1 / ($total_num_answers/$num_topics)) : 1;
 
                 // Add weighted difference to user profile. This weight decides the impact of a single article.
                 $scale = [-0.75, -0.25, 0, 0.25, 0.75];
                 $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+                // Add weighted difference (Is deducted if negative)
                 $user_profile[$j] += $weighted_diff;
 
-                // Prevent values above 100%
-                if($user_profile[$j] >= 1.0)
+                // Handle values below 0% and over 100%
+                if($user_profile[$j] > 1.0) {
+                    $weighted_diff -= ($user_profile[$j] - 1); // Remove value above 100% fr difference
                     $user_profile[$j] = 1.0;
+                } else if($user_profile[$j] < 0) {
+                    $weighted_diff += (0 - $user_profile[$j]); // Add value below 0% to difference
+                    $user_profile[$j] = 0;
+                }
 
-                // Normalize distribution on all other values
-                if($weighted_diff != 0) {
-                    $normalized_weighted_diff = $weighted_diff/($num_topics-1);
-                    for($i = 0; $i < $num_topics; $i++) {
-    
-                        // Normalize all values except current
-                        if($i != $j)
-                            $user_profile[$i] -= $normalized_weighted_diff;
-    
-                            // Prevent values below 0%
-                            if($user_profile[$i] < 0)
-                                $user_profile[$i] = 0;
+                // Calculate normalized difference
+                $normalized_weighted_diff = $weighted_diff / $other_num_topics;
+
+                // Distribute normalized difference over all other values
+                foreach(array_keys($user_profile) as $key) {
+
+                    // Ignore current value key
+                    if($key == $j)
+                        continue;
+
+                    // Remove normalized difference (Is added if negative)
+                    $user_profile[$key] -= $normalized_weighted_diff;
+
+                    // Handle normalized values above 100% and below 0%
+                    if($user_profile[$key] > 1.0) {
+                        $surp = ($user_profile[$key] - 1);
+
+                        // Retrieve first value which can add surplus from normalized value
+                        $compatibleValues = array_filter($user_profile, function($value, $key) use($surp, $j) {
+                            return $value + $surp < 1.0 && $key != $j;
+                        }, ARRAY_FILTER_USE_BOTH);
+                        $compatIndex = array_keys($compatibleValues, min($compatibleValues))[0];
+                        $user_profile[$compatIndex] += $surp;
+
+                        $user_profile[$key] = 1.0;
+                    } else if($user_profile[$key] < 0) {
+                        $surp = (0 - $user_profile[$key]);
+
+                        // Retrieve first value which can remove surplus from normalized value
+                        $compatibleValues = array_filter($user_profile, function($value, $key) use ($surp, $j) {
+                            return $value - $surp > 0 && $key != $j;
+                        }, ARRAY_FILTER_USE_BOTH);
+                        $compatIndex = array_keys($compatibleValues, min($compatibleValues))[0];
+                        $user_profile[$compatIndex] -= $surp;
+
+                        $user_profile[$key] = 0;
                     }
                 }
             }
 
+            // Save difference for each article
             if ($update) {
-                // Save difference for each article for each iteration
                 $article->pivot->difference = $total_diff;
                 $article->pivot->save();
             }
         }
+
 
         if ($update) {
             
@@ -96,7 +158,7 @@ class User extends Authenticatable
             $user->profile = $user_profile;
             $user->save();
 
-            return 1;
+            return "Distribution summary: ".(array_sum($user_profile) * 100)."%";
         }
         
         return $user_profile;
