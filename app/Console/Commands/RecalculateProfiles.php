dc2f34ab4add5f8a6450aee981468577aea22ec9
antonbergenudd <antonbergenudd@hotmail.com>
2022-04-08 13:59:52 +0200

    fix distributions

--- app/Console/Commands/RecalculateProfiles.php
+++ app/Console/Commands/RecalculateProfiles.php
@@ -39,67 +39,125 @@ class RecalculateProfiles extends Command
      */
     public function handle()
     {
-        foreach(User::all() as $j => $user) {
-            $iteration = 0; # Set iteration number
+        dump('Version 4');
+        $version = 4;
 
-            $articles = $user->answers()->get()->where('iteration_id', $iteration)->sortByDesc('id');
+        dump('Iteration 0');
+        $iteration = 0; # Set iteration number
+        $iteration_0_users = User::whereHas('profile', function($q) use($iteration) { 
+            $q->where('iteration_id', $iteration);
+        })->get();
+        foreach($iteration_0_users as $user) {
+            if($version == 1 || $version == 4) {
+                $answered_articles = $user->answers()->get()->where('iteration_id', $iteration)->sortByDesc('id');
+            } else if($version == 2 || $version == 3) {
+                $answered_articles = $user->answers()->get()->where('iteration_id', $iteration);
+            }
 
-            if(count($articles) > 0) {
-                $num_topics = count(json_decode($articles->first()->label_dist)); # Calc number of topics
-                $total_num_answers = count($articles);
-                // $user_profile = $this->latestProfile(); # Retrieve latest user profile
-                
-                # Reset user profile
+            if(count($answered_articles) > 0) {
+                $num_topics = count(json_decode($answered_articles->first()->label_dist)); # Calc number of topics
+                $num_answers = count($answered_articles);
                 $user_profile = array_fill(0, $num_topics, (100/$num_topics)/100);
-                
-                // Loop all articles and compute average label dist for user profile
-                foreach($articles as $i => $article) {
 
-                    // Get article distribution
-                    $article_dist = json_decode($article->label_dist);
+                if($version == 1 || $version == 2) {
+                    
+                    // Loop all articles and compute average label dist for user profile
+                    foreach($answered_articles as $i => $article) {
 
-                    // Add each weighted label distribution over an article to collected array (rel score * single label dist)
-                    $total_diff = [];
-                    foreach($article_dist as $j => $single_topic_dist) {
-                        $userArraySum = array_sum($user_profile);
+                        // Get article distribution
+                        $article_dist = json_decode($article->label_dist);
 
-                        // Get abs. difference between profiles
-                        $diff = $single_topic_dist - $user_profile[$j];
-                        $total_diff[] = $diff;
+                        // Add each weighted label distribution over an article to collected array (rel score * single label dist)
+                        $total_diff = [];
+                        foreach($article_dist as $j => $single_topic_dist) {
+                            $userArraySum = array_sum($user_profile);
 
-                        // Decrease impact of articles the more you answer
-                        // Roughly every other iteration (30 articles)
-                        $gradient = $total_num_answers >= 30 ? (1 / ($total_num_answers/$num_topics)) : 1;
+                            // Get abs. difference between profiles
+                            $diff = $single_topic_dist - $user_profile[$j];
+                            $total_diff[] = $diff;
 
-                        // Add weighted difference to user profile. This weight decides the impact of a single article.
-                        $scale = [-0.75, -0.25, 0, 0.25, 0.75];
-                            
-                        $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+                            // Decrease impact of articles the more you answer
+                            // Roughly every other iteration (30 articles)
+                            $gradient = $num_answers >= 30 ? (1 / ($num_answers/$num_topics)) : 1;
 
-                        // Add weighted difference (Is deducted if negative)
-                        $user_profile[$j] += $weighted_diff;
+                            // Add weighted difference to user profile. This weight decides the impact of a single article.
+                            $scale = [-0.75, -0.25, 0, 0.25, 0.75];
+                                
+                            $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+                            // Add weighted difference (Is deducted if negative)
+                            $user_profile[$j] += $weighted_diff;
+                        }
+
+                        // Alter values to be between 0 and 1
+                        $max = max($user_profile); $min = min($user_profile);
+                        if($max != $min) {
+                            // normalized = (x-min(x))/(max(x)-min(x))
+                            foreach($user_profile as $j => $profile_val) {
+                                $user_profile[$j] = ($profile_val - $min) / ($max - $min);
+                            }
+
+                            // Normalize values
+                            $sum = array_sum($user_profile);
+                            foreach($user_profile as $j => $profile_val) {
+                                $user_profile[$j] = $profile_val / $sum;
+                            }
+                        }
+                    }
+                } else if($version == 3 || $version == 4) {
+                    $sum_user_profile = (array) clone((object) $user_profile);
+
+                    // Loop all articles and compute average label dist for user profile
+                    foreach($answered_articles as $i => $article) {
+
+                        // Get article distribution
+                        $article_dist = json_decode($article->label_dist);
+
+                        // Add each weighted label distribution over an article to collected array (rel score * single label dist)
+                        $total_diff = [];
+                        foreach($article_dist as $j => $single_topic_dist) {
+
+                            // Get abs. difference between profiles
+                            $diff = $single_topic_dist - $user_profile[$j];
+                            $total_diff[] = $diff;
+
+                            // Decrease impact of articles the more you answer
+                            // Roughly every other iteration (30 articles)
+                            $gradient = $num_answers >= 30 ? (1 / ($num_answers/$num_topics)) : 1;
+
+                            // Add weighted difference to user profile. This weight decides the impact of a single article.
+                            $scale = [-0.75, -0.25, 0, 0.25, 0.75];
+                                
+                            $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+                            // Add weighted difference (Is deducted if negative)
+                            $sum_user_profile[$j] += $weighted_diff;
+                        }
                     }
 
-                    // Alter values to be between 0 and 1
-                    $max = max($user_profile); $min = min($user_profile);
-                    if($max != $min) {
+                    if($sum_user_profile != $user_profile) {
+                        foreach($sum_user_profile as $j => $val) {
+                            $user_profile[$j] = $val / $num_answers;
+                        }
+    
+                        // Alter values to be between 0 and 1
+                        $max = max($user_profile); $min = min($user_profile);
+
                         // normalized = (x-min(x))/(max(x)-min(x))
-                        foreach($user_profile as $j => $profile_val) {
-                            $user_profile[$j] = ($profile_val - $min) / ($max - $min);
+                        foreach($user_profile as $j => $val) {
+                            $user_profile[$j] = ($val - $min) / ($max - $min);
                         }
 
                         // Normalize values
                         $sum = array_sum($user_profile);
-                        foreach($user_profile as $j => $profile_val) {
-                            $user_profile[$j] = $profile_val / $sum;
+                        foreach($user_profile as $j => $val) {
+                            $user_profile[$j] = $val / $sum;
                         }
                     }
                 }
 
                 // Remove old profile
                 UserProfile::where('user_id', $user->id)->where('iteration_id', $iteration)->delete();
-
-                dump('Creating updated profile for user '.$user->id.'.');
 
                 // Create new profile
                 $userProfile = new UserProfile;
@@ -108,6 +166,141 @@ class RecalculateProfiles extends Command
                 $userProfile->profile = json_encode($user_profile);
                 $userProfile->iteration_id = $iteration;
                 $userProfile->save();
+
+                dump('Creating updated profile for user '.$user->id.'.');
+                dump('Sum of profile '. array_sum($user_profile));
+            } else {
+                dump('No user answers found');
+            }
+        }
+
+        dump('Iteration 1');
+        $iteration = 1;
+        $iteration_1_users = User::whereHas('profile', function($q) use($iteration) { 
+                $q->where('iteration_id', $iteration - 1);
+            })->get();
+        foreach($iteration_1_users as $user) {
+            if($version == 1 || $version == 4) {
+                $answered_articles = $user->answers()->get()->where('iteration_id', $iteration)->sortByDesc('id');
+            } else if($version == 2 || $version == 3) {
+                $answered_articles = $user->answers()->get()->where('iteration_id', $iteration);
+            }
+
+            if(count($answered_articles) > 0) {
+                $num_topics = count(json_decode($answered_articles->first()->label_dist)); # Calc number of topics
+                $num_answers = count($answered_articles);
+                $user_profile = json_decode($user->iterationProfile($iteration - 1)->profile);
+
+                if($version == 1 || $version == 2) {
+                    
+                    // Loop all articles and compute average label dist for user profile
+                    foreach($answered_articles as $i => $article) {
+
+                        // Get article distribution
+                        $article_dist = json_decode($article->label_dist);
+
+                        // Add each weighted label distribution over an article to collected array (rel score * single label dist)
+                        $total_diff = [];
+                        foreach($article_dist as $j => $single_topic_dist) {
+                            $userArraySum = array_sum($user_profile);
+
+                            // Get abs. difference between profiles
+                            $diff = $single_topic_dist - $user_profile[$j];
+                            $total_diff[] = $diff;
+
+                            // Decrease impact of articles the more you answer
+                            // Roughly every other iteration (30 articles)
+                            $gradient = $num_answers >= 30 ? (1 / ($num_answers/$num_topics)) : 1;
+
+                            // Add weighted difference to user profile. This weight decides the impact of a single article.
+                            $scale = [-0.75, -0.25, 0, 0.25, 0.75];
+                                
+                            $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+                            // Add weighted difference (Is deducted if negative)
+                            $user_profile[$j] += $weighted_diff;
+                        }
+
+                        // Alter values to be between 0 and 1
+                        $max = max($user_profile); $min = min($user_profile);
+                        if($max != $min) {
+                            // normalized = (x-min(x))/(max(x)-min(x))
+                            foreach($user_profile as $j => $profile_val) {
+                                $user_profile[$j] = ($profile_val - $min) / ($max - $min);
+                            }
+
+                            // Normalize values
+                            $sum = array_sum($user_profile);
+                            foreach($user_profile as $j => $profile_val) {
+                                $user_profile[$j] = $profile_val / $sum;
+                            }
+                        }
+                    }
+                } else if($version == 3 || $version == 4) {
+                    $sum_user_profile = (array) clone((object) $user_profile);
+
+                    // Loop all articles and compute average label dist for user profile
+                    foreach($answered_articles as $i => $article) {
+
+                        // Get article distribution
+                        $article_dist = json_decode($article->label_dist);
+
+                        // Add each weighted label distribution over an article to collected array (rel score * single label dist)
+                        $total_diff = [];
+                        foreach($article_dist as $j => $single_topic_dist) {
+
+                            // Get abs. difference between profiles
+                            $diff = $single_topic_dist - $user_profile[$j];
+                            $total_diff[] = $diff;
+
+                            // Decrease impact of articles the more you answer
+                            // Roughly every other iteration (30 articles)
+                            $gradient = $num_answers >= 30 ? (1 / ($num_answers/$num_topics)) : 1;
+
+                            // Add weighted difference to user profile. This weight decides the impact of a single article.
+                            $scale = [-0.75, -0.25, 0, 0.25, 0.75];
+                                
+                            $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+                            // Add weighted difference (Is deducted if negative)
+                            $sum_user_profile[$j] += $weighted_diff;
+                        }
+                    }
+
+                    if($sum_user_profile != $user_profile) {
+                        foreach($sum_user_profile as $j => $val) {
+                            $user_profile[$j] = $val / $num_answers;
+                        }
+    
+                        // Alter values to be between 0 and 1
+                        $max = max($user_profile); $min = min($user_profile);
+
+                        // normalized = (x-min(x))/(max(x)-min(x))
+                        foreach($user_profile as $j => $val) {
+                            $user_profile[$j] = ($val - $min) / ($max - $min);
+                        }
+
+                        // Normalize values
+                        $sum = array_sum($user_profile);
+                        foreach($user_profile as $j => $val) {
+                            $user_profile[$j] = $val / $sum;
+                        }
+                    }
+                }
+
+                // Remove old profile
+                UserProfile::where('user_id', $user->id)->where('iteration_id', $iteration)->delete();
+
+                // Create new profile
+                $userProfile = new UserProfile;
+                $userProfile->user_id = $user->id;
+                $userProfile->token = $user->token;
+                $userProfile->profile = json_encode($user_profile);
+                $userProfile->iteration_id = $iteration;
+                $userProfile->save();
+
+                dump('Creating updated profile for user '.$user->id.'.');
+                dump('Sum of profile '. array_sum($user_profile));
             } else {
                 dump('No user answers found');
             }
--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,14 @@ class User extends Authenticatable
         return $this->hasMany(UserProfile::class)->where('iteration_id', $iteration)->first();
     }
 
+     /**
+     * Retrieve all articles related to user
+     */
+    public function profile()
+    {
+        return $this->hasMany(UserProfile::class);
+    }
+
     /**
      * Retrieve all articles related to user
      */
@@ -46,10 +54,10 @@ class User extends Authenticatable
         return $this->hasMany(UserProfile::class)->get()->sortByDesc('iteration_id')->first();
     }
 
-    public function updateProfile($articles) {
-        $num_topics = count(json_decode($articles->first()->label_dist)); # Calc number of topics
-        $iteration = $articles->sortByDesc('iteration_id')->first()->iteration_id; # Set iteration number
-        $total_num_answers = count($articles);
+    public function updateProfile($answered_articles) {
+        $num_topics = count(json_decode($answered_articles->first()->label_dist)); # Calc number of topics
+        $iteration = $answered_articles->sortByDesc('iteration_id')->first()->iteration_id; # Set iteration number
+        $num_answers = count($answered_articles);
         $user_profile = $this->latestProfile(); # Retrieve latest user profile
         
         # Init unit distributed user profile if not set before
@@ -57,9 +65,10 @@ class User extends Authenticatable
             $user_profile = array_fill(0, $num_topics, (100/$num_topics)/100);
         else
             $user_profile = json_decode($user_profile->profile);
-        
+
         // Loop all articles and compute average label dist for user profile
-        foreach($articles as $i => $article) {
+        $sum_user_profile = (array) clone((object) $user_profile);
+        foreach($answered_articles as $i => $article) {
 
             // Get article distribution
             $article_dist = json_decode($article->label_dist);
@@ -67,7 +76,6 @@ class User extends Authenticatable
             // Add each weighted label distribution over an article to collected array (rel score * single label dist)
             $total_diff = [];
             foreach($article_dist as $j => $single_topic_dist) {
-                $userArraySum = array_sum($user_profile);
 
                 // Get abs. difference between profiles
                 $diff = $single_topic_dist - $user_profile[$j];
@@ -75,7 +83,7 @@ class User extends Authenticatable
 
                 // Decrease impact of articles the more you answer
                 // Roughly every other iteration (30 articles)
-                $gradient = $total_num_answers >= 30 ? (1 / ($total_num_answers/$num_topics)) : 1;
+                $gradient = $num_answers >= 30 ? (1 / ($num_answers/$num_topics)) : 1;
 
                 // Add weighted difference to user profile. This weight decides the impact of a single article.
                 $scale = [-0.75, -0.25, 0, 0.25, 0.75];
@@ -83,39 +91,111 @@ class User extends Authenticatable
                 $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
 
                 // Add weighted difference (Is deducted if negative)
-                $user_profile[$j] += $weighted_diff;
+                $sum_user_profile[$j] += $weighted_diff;
+            }
+        }
+
+        if($sum_user_profile != $user_profile) {
+            // Take average of all article differences
+            foreach($sum_user_profile as $j => $val) {
+                $user_profile[$j] = $val / $num_answers;
             }
 
             // Alter values to be between 0 and 1
-            $max = max($user_profile);
-            $min = min($user_profile);
-            foreach($user_profile as $j => $profile_val) {
-                $user_profile[$j] = ($profile_val - $min) / ($max - $min);
+            $max = max($user_profile); $min = min($user_profile);
+
+            // normalized = (x-min(x))/(max(x)-min(x))
+            foreach($user_profile as $j => $val) {
+                $user_profile[$j] = ($val - $min) / ($max - $min);
             }
 
             // Normalize values
             $sum = array_sum($user_profile);
-            foreach($user_profile as $j => $profile_val) {
-                $user_profile[$j] = $profile_val / $sum;
+            foreach($user_profile as $j => $val) {
+                $user_profile[$j] = $val / $sum;
             }
-
-            // Save difference for each article
-            $article->pivot->difference = $total_diff;
-            $article->pivot->save();
         }
 
-        // Add updated user profile log
-        if (!UserProfile::where('user_id', $this->id)->where('iteration_id', $iteration)->exists()) {
-            $userProfile = new UserProfile;
-            $userProfile->user_id = $this->id;
-            $userProfile->token = $this->token;
-            $userProfile->profile = json_encode($user_profile);
-            $userProfile->iteration_id = $iteration;
-            $userProfile->save();
-        }
+        // Create new profile
+        $userProfile = new UserProfile;
+        $userProfile->user_id = $this->id;
+        $userProfile->token = $this->token;
+        $userProfile->profile = json_encode($user_profile);
+        $userProfile->iteration_id = $iteration;
+        $userProfile->save();
 
         return "Distribution summary: ".(array_sum($user_profile) * 100)."%";
     }
+    // public function updateProfile($articles) {
+    //     $num_topics = count(json_decode($articles->first()->label_dist)); # Calc number of topics
+    //     $iteration = $articles->sortByDesc('iteration_id')->first()->iteration_id; # Set iteration number
+    //     $total_num_answers = count($articles);
+    //     $user_profile = $this->latestProfile(); # Retrieve latest user profile
+        
+    //     # Init unit distributed user profile if not set before
+    //     if (! isset($user_profile))
+    //         $user_profile = array_fill(0, $num_topics, (100/$num_topics)/100);
+    //     else
+    //         $user_profile = json_decode($user_profile->profile);
+        
+    //     // Loop all articles and compute average label dist for user profile
+    //     foreach($articles as $i => $article) {
+
+    //         // Get article distribution
+    //         $article_dist = json_decode($article->label_dist);
+
+    //         // Add each weighted label distribution over an article to collected array (rel score * single label dist)
+    //         $total_diff = [];
+    //         foreach($article_dist as $j => $single_topic_dist) {
+    //             $userArraySum = array_sum($user_profile);
+
+    //             // Get abs. difference between profiles
+    //             $diff = $single_topic_dist - $user_profile[$j];
+    //             $total_diff[] = $diff;
+
+    //             // Decrease impact of articles the more you answer
+    //             // Roughly every other iteration (30 articles)
+    //             $gradient = $total_num_answers >= 30 ? (1 / ($total_num_answers/$num_topics)) : 1;
+
+    //             // Add weighted difference to user profile. This weight decides the impact of a single article.
+    //             $scale = [-0.75, -0.25, 0, 0.25, 0.75];
+                    
+    //             $weighted_diff = ($diff != 0 ? $diff * $scale[$article->pivot->relevance - 1] * $gradient : 0);
+
+    //             // Add weighted difference (Is deducted if negative)
+    //             $user_profile[$j] += $weighted_diff;
+    //         }
+
+    //         // Alter values to be between 0 and 1
+    //         $max = max($user_profile);
+    //         $min = min($user_profile);
+    //         foreach($user_profile as $j => $profile_val) {
+    //             $user_profile[$j] = ($profile_val - $min) / ($max - $min);
+    //         }
+
+    //         // Normalize values
+    //         $sum = array_sum($user_profile);
+    //         foreach($user_profile as $j => $profile_val) {
+    //             $user_profile[$j] = $profile_val / $sum;
+    //         }
+
+    //         // Save difference for each article
+    //         $article->pivot->difference = $total_diff;
+    //         $article->pivot->save();
+    //     }
+
+    //     // Add updated user profile log
+    //     if (!UserProfile::where('user_id', $this->id)->where('iteration_id', $iteration)->exists()) {
+    //         $userProfile = new UserProfile;
+    //         $userProfile->user_id = $this->id;
+    //         $userProfile->token = $this->token;
+    //         $userProfile->profile = json_encode($user_profile);
+    //         $userProfile->iteration_id = $iteration;
+    //         $userProfile->save();
+    //     }
+
+    //     return "Distribution summary: ".(array_sum($user_profile) * 100)."%";
+    // }
 
     // public function updateProfile($articles) {
     //     $num_topics = count(json_decode($articles->first()->label_dist)); # Calc number of topics
