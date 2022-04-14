81ad3083b0273eb5536180afb51b9e98fb4c030a
antonbergenudd <antonbergenudd@hotmail.com>
2022-04-14 13:34:34 +0200

    allow ip

--- app/Http/Middleware/ValidateAdminIP.php
+++ app/Http/Middleware/ValidateAdminIP.php
@@ -18,7 +18,8 @@ class ValidateAdminIP
     {
         $ip_addresses = [
             '127.0.0.1',
-            '83.249.247.108'
+            '83.249.247.108',
+            '82.147.62.178' # skistar wifi..
         ];
 
         if(! in_array($request->ip(), $ip_addresses)) {
